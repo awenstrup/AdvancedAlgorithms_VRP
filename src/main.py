4c11521829b2af055a5112488856f3466cf31d90
Alex Wenstrup <awenstrup@olin.edu>
2021-04-23 09:47:22 -0400

    Update load_warehouse to expect .txt file, robot base station instead of start/end

--- src/main.py
+++ src/main.py
@@ -1,1 +1,6 @@
 from path import Path
+from utils import Warehouse
+
+if __name__ == "__main__":
+    w1 = Warehouse("warehouse.txt")
+    p = Path(w1)
--- src/utils.py
+++ src/utils.py
@@ -4,10 +4,15 @@ from typing import Tuple, List
 class Warehouse:
     """A small utility class to hold information about the warehouse we are opperating in"""
 
-    def __init__(self, path):
-        self.occupancy_grid, self.start, self.end = self.load_warehouse(path)
+    def __init__(self, path: str):
+        if ".txt" in path:
+            self.occupancy_grid, self.base = self.load_warehouse(path)
+        elif ".png" in path:
+            self.occupancy_grid, self.base = self.load_warehouse_from_png(path)
+        else:
+            raise Exception("Warehouse must be defined in a txt or png file")
 
-    def load_warehouse(self, path: str) -> Tuple[List[List[bool]], Tuple[int, int], Tuple[int, int]]:
+    def load_warehouse(self, path: str) -> Tuple[List[List[bool]], Tuple[int, int]]:
         """Given a path to a warehouse file, load it and return the useful information.
 
         :param str path: The path (hopefully absolute) to the .txt file representing the warehouse.
@@ -15,25 +20,28 @@ class Warehouse:
 
         :returns:
             * A 2D list of booleans; True spaces are occupied, False spaces can be traveled through.
-            * A start point for a single robot
-            * An end point for a single robot
+            * A base point for a single robot
         """
         occupancy_grid: list = []
-        start: tuple
-        end: tuple
+        base: tuple
 
         with open(path) as f:
             for i, line in enumerate(f.readlines()):
                 for j, char in enumerate(line):
                     if char == "X":
-                        occupancy_grid[i][j] = True
+                        occupancy_grid[i].append(True)
                     elif char == " ":
-                        occupancy_grid[i][j] = False
-                    elif char == "A":
-                        occupancy_grid[i][j] = False
-                        start = (i, j)
+                        occupancy_grid[i].append(False)
                     elif char == "B":
-                        occupancy_grid[i][j] = False
-                        end = (i, j)
+                        occupancy_grid[i].append(False)
+                        base = (i, j)
 
         return occupancy_grid, start, end
+
+    def load_warehouse_from_png(path: str) -> Tuple[List[List[bool]], Tuple[int, int], Tuple[int, int]]:
+        """Given a path to a PNG, load a warehouse.
+
+        Expect obstacles in black, empty space in white, and robot charging
+        stations in red
+        """
+        pass
