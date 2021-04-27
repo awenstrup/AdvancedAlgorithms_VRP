5efb76f424dd2b1952d05cfc0c07e3ca6b6c0d21
Alex Wenstrup <awenstrup@olin.edu>
2021-04-27 09:05:08 -0400

    Make a cooler gif

--- src/main.py
+++ src/main.py
@@ -2,14 +2,18 @@ from path import Path, save_multiple_paths_as_gif
 from utils import Warehouse
 
 if __name__ == "__main__":
-    w1 = Warehouse("warehouse.png")
+    w1 = Warehouse("warehouse.txt")
     # w1.write_warehouse_to_png("warehouse.png")
-    p1 = Path(w1, w1.bases[0], battery_life=20)
-    p2 = Path(w1, w1.bases[1], battery_life=20)
+    p1 = Path(w1, w1.bases[0], battery_life=30)
+    p2 = Path(w1, w1.bases[1], battery_life=30)
+    p3 = Path(w1, w1.bases[2], battery_life=30)
+    p4 = Path(w1, w1.bases[3], battery_life=30)
 
     for i in range(5000):
         p1.mutate()
         p2.mutate()
+        p3.mutate()
+        p4.mutate()
         # print(p.coord_list)
 
-    save_multiple_paths_as_gif(5, [p1, p2])
+    save_multiple_paths_as_gif(10, [p1, p2, p3, p4])
