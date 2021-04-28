951102f08bd42345f7ce95fc0a1da4c24fbe8663
SanderMiller <amiller@olin.edu>
2021-04-28 11:26:45 -0400

    First implementation of local search

--- src/main.py
+++ src/main.py
@@ -1,24 +1,35 @@
 from path import Path, save_multiple_paths_as_gif
 from utils import Warehouse
+import copy
 
 if __name__ == "__main__":
     w1 = Warehouse("warehouse.txt")
     # w1.write_warehouse_to_png("warehouse.png")
-    p1 = Path(w1, w1.bases[0], battery_life=30)
-    p2 = Path(w1, w1.bases[1], battery_life=30)
-    p3 = Path(w1, w1.bases[2], battery_life=30)
-    p4 = Path(w1, w1.bases[3], battery_life=30)
-
-    for i in range(5000):
-        p1.mutate()
-        p2.mutate()
-        p3.mutate()
-        p4.mutate()
-        # print(p.coord_list)
-  
-    p2.fitness_func(2)
-    p3.fitness_func(2)
-    p4.fitness_func(2)
+    p1 = Path(w1, w1.bases[3], battery_life=150)
+    #p2 = Path(w1, w1.bases[1], battery_life=30)
+    #p3 = Path(w1, w1.bases[2], battery_life=30)
+    #p4 = Path(w1, w1.bases[3], battery_life=30)
     p1.fitness_func(2)
+    print(p1.fitness_val)
+    for i in range(10000):
+        pMutated = copy.deepcopy(p1)
+        pMutated.mutate()
+        pMutated.fitness_func(2)
+        print(p1.fitness_val)
+        
 
-    save_multiple_paths_as_gif(10, [p1, p2, p3, p4])
+        if pMutated.fitness_val>p1.fitness_val:
+            print(pMutated.fitness_val)
+            p1 = copy.deepcopy(pMutated)
+            p1.fitness_func(2)
+            
+       
+        
+       
+        # print(p.coord_list)
+    p1.fitness_func(2)
+    print(p1.fitness_val)
+   
+   
+
+    save_multiple_paths_as_gif(10, [p1])
