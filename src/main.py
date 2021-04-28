97e58c9ef5d781580293d02b919dceb7d5598491
SanderMiller <amiller@olin.edu>
2021-04-28 17:08:00 -0400

    Messing with tuning

--- src/main.py
+++ src/main.py
@@ -5,9 +5,9 @@ from progress.bar import Bar
 import random
 
 if __name__ == "__main__":
-    w1 = Warehouse("warehouse1.txt")
+    w1 = Warehouse("bigWarehouse.txt")
     # w1.write_warehouse_to_png("warehouse.png")
-    p1 = Path(w1, w1.bases[0], battery_life=20)
+    p1 = Path(w1, w1.bases[0], battery_life=30)
     #p2 = Path(w1, w1.bases[1], battery_life=30)
     #p3 = Path(w1, w1.bases[2], battery_life=30)
     #p4 = Path(w1, w1.bases[3], battery_life=30)
@@ -19,11 +19,12 @@ if __name__ == "__main__":
     for i in range(cycles):
         pMutated = copy.deepcopy(p1)
         pMutated.mutate()
+        pMutated.distance_multiplier = .01*(i/cycles)
         pMutated.fitness_func(1)
         # print(p1.fitness_val)
         
 
-        if pMutated.fitness_val>p1.fitness_val or (random.random() > 0.8):
+        if pMutated.fitness_val>p1.fitness_val or (random.random() > i/cycles):
             print(pMutated.fitness_val)
             p1 = copy.deepcopy(pMutated)
             p1.fitness_func(1)
