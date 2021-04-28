cecba5ffa8e472d780915b1327c01fa0bb7c9c43
SanderMiller <amiller@olin.edu>
2021-04-28 12:00:11 -0400

    Updated local Search

--- src/main.py
+++ src/main.py
@@ -5,13 +5,13 @@ import copy
 if __name__ == "__main__":
     w1 = Warehouse("warehouse.txt")
     # w1.write_warehouse_to_png("warehouse.png")
-    p1 = Path(w1, w1.bases[3], battery_life=150)
+    p1 = Path(w1, w1.bases[3], battery_life=10)
     #p2 = Path(w1, w1.bases[1], battery_life=30)
     #p3 = Path(w1, w1.bases[2], battery_life=30)
     #p4 = Path(w1, w1.bases[3], battery_life=30)
     p1.fitness_func(2)
     print(p1.fitness_val)
-    for i in range(10000):
+    for i in range(8000):
         pMutated = copy.deepcopy(p1)
         pMutated.mutate()
         pMutated.fitness_func(2)
@@ -22,12 +22,14 @@ if __name__ == "__main__":
             print(pMutated.fitness_val)
             p1 = copy.deepcopy(pMutated)
             p1.fitness_func(2)
+        
             
        
         
        
         # print(p.coord_list)
     p1.fitness_func(2)
+    print(p1.coord_list)
     print(p1.fitness_val)
    
    
