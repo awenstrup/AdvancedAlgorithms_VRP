a8dfd52f00fa913a10574ac69d1eacba98c51a26
Alex Wenstrup <awenstrup@olin.edu>
2021-04-28 11:52:10 -0400

    Add progress bar to mutation process

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from path import Path, save_multiple_paths_as_gif
 from utils import Warehouse
 import copy
+from progress.bar import Bar
 
 if __name__ == "__main__":
     w1 = Warehouse("warehouse.txt")
@@ -11,7 +12,10 @@ if __name__ == "__main__":
     #p4 = Path(w1, w1.bases[3], battery_life=30)
     p1.fitness_func(2)
     print(p1.fitness_val)
-    for i in range(10000):
+
+    cycles = 2500
+    bar = Bar('Mutating', max=cycles)
+    for i in range(cycles):
         pMutated = copy.deepcopy(p1)
         pMutated.mutate()
         pMutated.fitness_func(2)
@@ -22,11 +26,12 @@ if __name__ == "__main__":
             print(pMutated.fitness_val)
             p1 = copy.deepcopy(pMutated)
             p1.fitness_func(2)
-            
-       
-        
+
+        bar.next()
        
         # print(p.coord_list)
+        
+    bar.finish()
     p1.fitness_func(2)
     print(p1.fitness_val)
    
