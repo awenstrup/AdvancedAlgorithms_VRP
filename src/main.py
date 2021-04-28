6e121e795f466fddaca31d1764d16cce21b9a662
SanderMiller <amiller@olin.edu>
2021-04-28 12:06:53 -0400

    Merging

--- src/main.py
+++ src/main.py
@@ -12,14 +12,10 @@ if __name__ == "__main__":
     #p4 = Path(w1, w1.bases[3], battery_life=30)
     p1.fitness_func(2)
     print(p1.fitness_val)
-<<<<<<< HEAD
-    for i in range(8000):
-=======
 
     cycles = 2500
     bar = Bar('Mutating', max=cycles)
     for i in range(cycles):
->>>>>>> a8dfd52f00fa913a10574ac69d1eacba98c51a26
         pMutated = copy.deepcopy(p1)
         pMutated.mutate()
         pMutated.fitness_func(2)
@@ -30,15 +26,8 @@ if __name__ == "__main__":
             print(pMutated.fitness_val)
             p1 = copy.deepcopy(pMutated)
             p1.fitness_func(2)
-<<<<<<< HEAD
-        
-            
-       
-        
-=======
 
         bar.next()
->>>>>>> a8dfd52f00fa913a10574ac69d1eacba98c51a26
        
         # print(p.coord_list)
         
